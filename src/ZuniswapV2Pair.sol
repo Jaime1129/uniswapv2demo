7528af89a04f4518b44b375b2dfa3a6dd874a9b2
jaime wang <greatjaime.wang@gmail.com>
2024-04-28 10:53:27 +0800

    implement swap (without fees)

--- src/ZuniswapV2Pair.sol
+++ src/ZuniswapV2Pair.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import "./util/Math.sol";
 import "solmate/tokens/ERC20.sol";
+import "./util/UQ112x112.sol";
 
 interface IERC20 {
     function balanceOf(address) external returns (uint256);
@@ -13,8 +14,14 @@ interface IERC20 {
 error InsufficientLiquidityMinted();
 error InsufficientLiquidityBurned();
 error TransferFailed();
+error InsufficientOutputAmount();
+error InsufficientLiquidity();
+error InvalidK();
+error BalanceOverflow();
 
 contract ZUniSwapV2Pair is ERC20, Math {
+    using UQ112x112 for uint224;
+
     uint256 constant MINIMUM_LIQUIDITY = 1000;
 
     address public token0;
@@ -24,6 +31,12 @@ contract ZUniSwapV2Pair is ERC20, Math {
     uint112 private reserve0;
     uint112 private reserve1;
 
+    // timestamp of last swapping
+    uint32 private blockTimestampLast;
+    // delta price * time elapsed since last swapping
+    uint256 public price0CumulativeLast;
+    uint256 public price1CumulativeLast;
+
     constructor(address _token0, address _token1) ERC20("ZUniSwapV2Pair", "ZUNIV2", 18) {
         token0 = _token0;
         token1 = _token1;
@@ -32,6 +45,7 @@ contract ZUniSwapV2Pair is ERC20, Math {
     event Burn(address indexed sender, uint256 amount0, uint256 amount1);
     event Mint(address indexed sender, uint256 amount0, uint256 amount1);
     event Sync(uint256 reserve0, uint256 reserve1);
+    event Swap(address indexed sender, uint256 amount0, uint256 amount1, address to);
 
     // mint LP tokens when adding liquidity
     function mint() public {
@@ -93,9 +107,40 @@ contract ZUniSwapV2Pair is ERC20, Math {
         // update reserve
         balance0 = IERC20(token0).balanceOf(address(this));
         balance1 = IERC20(token1).balanceOf(address(this));
-        _update(balance0, balance1);
+
+        (uint112 reserve0_, uint112 reserve1_) = getReserves();
+        _update(balance0, balance1, reserve0_, reserve1_);
 
         emit Burn(msg.sender, amount0, amount1);
+    }
+
+    function swap(
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address to
+    ) public {
+        if (amount0Out <= 0 || amount1Out <= 0) {
+            revert InsufficientOutputAmount();
+        }
+        (uint112 reserve0_, uint112 reserve1_) = getReserves();
+        if (amount0Out > reserve0_ || amount1Out > reserve1_) {
+            revert InsufficientLiquidity();
+        }
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this)) - amount0Out;
+        uint256 balance1 = IERC20(token1).balanceOf(address(this)) - amount1Out;
+
+        // the new constant K must equal or larger than current one
+        if (balance0 * balance1 < uint256(reserve0_) * uint256(reserve1_)) {
+            revert InvalidK();
+        }
+
+        _update(balance0, balance1, reserve0_, reserve1_);
+
+        if (amount0Out > 0) _safeTranser(token0, to, amount0Out);
+        if (amount1Out > 0) _safeTranser(token1, to, amount1Out);
+
+        emit Swap(msg.sender, amount0Out, amount1Out, to);
     }
 
     function getReserves() public view returns (uint112 _reserve0, uint112 _reserve1) {
@@ -108,9 +153,31 @@ contract ZUniSwapV2Pair is ERC20, Math {
     // 
 
     // update reserve amount
-    function _update(uint256 balance0, uint256 balance1) internal {
+    function _update(
+        uint256 balance0, 
+        uint256 balance1,
+        uint112 reserve0_,
+        uint112 reserve1_
+    ) internal {
+        if (balance0 > type(uint112).max || balance1 > type(uint112).max) {
+            revert BalanceOverflow();
+        }
+
+        // The unchecked block in Solidity allows developers to bypass these automatic overflow and underflow checks within the scope of the block. 
+        // This can be useful for optimizing gas costs in situations where the developer is certain that overflows or underflows cannot occur, 
+        // or when such conditions are intended or managed manually.
+        unchecked {
+            uint32 timeElapsed = uint32(block.timestamp) - blockTimestampLast;
+            if (timeElapsed > 0 && reserve0_ > 0 && reserve1_ > 0) {
+                price0CumulativeLast = uint256(UQ112x112.encode(reserve1_).uqdiv(reserve0_)) * timeElapsed;
+                price1CumulativeLast = uint256(UQ112x112.encode(reserve0_).uqdiv(reserve1_)) * timeElapsed;
+            }
+        }
+
         reserve0 = uint112(balance0);
         reserve1 = uint112(balance1);
+        blockTimestampLast = uint32(block.timestamp);
+
         emit Sync(reserve0, reserve1);
     }
 
